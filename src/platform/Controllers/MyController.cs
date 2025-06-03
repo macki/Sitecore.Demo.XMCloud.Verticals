6dc5d3942835419c9730d36bb35bbb28e82649e0
Lukasz Macura <lmacura@softserveinc.com>
2025-06-03 16:02:22 +0200

    0: Add Controller

---
+++ src/platform/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XmCloudSXAStarter.Controllers
+{
+    public class MyController : SitecoreController
+    {
+        public ActionResult MyAction()
+        {
+            // Your controller logic here
+            return Json(new { success = true, message = "Hello from XM Cloud controller" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
