de47667377d1a7dc313f6a284431e0121629173b
Lukasz Macura <lmacura@softserveinc.com>
2025-06-03 16:09:11 +0200

    0: PR

--- src/platform/Controllers/MyController.cs
+++ src/platform/Controllers/MyController.cs
@@ -1,8 +1,5 @@
 ﻿using Sitecore.Mvc.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 using System.Web.Mvc;
 
 namespace XmCloudSXAStarter.Controllers
